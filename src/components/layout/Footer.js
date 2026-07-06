6045a20d5e69678b6d06702f06b146894a49511d
Darron Hartas <darron.hartas@gmail.com>
2026-07-06 22:07:45 +0100

    chore: bump version to 2.0.1

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Footer = ({ onExport, onImportClick, onSyncClick, syncStatus, isAuthed, version = '2.0.0' }) => {
+const Footer = ({ onExport, onImportClick, onSyncClick, syncStatus, isAuthed, version = '2.0.1' }) => {
     const styles = {
         footer: {
             flexShrink: 0,
